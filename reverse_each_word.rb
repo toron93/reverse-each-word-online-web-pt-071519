4453c92f3e593eef3b9c7f0901c48255960b205c
Toron Seigler <toronseigler9@gmail.com>
2019-08-05 18:42:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,9 @@
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
-  end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
